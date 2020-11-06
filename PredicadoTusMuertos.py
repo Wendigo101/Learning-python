f6d3153c72d85860871b974a9c8286637ca56172
Daniel Lopez Aires <64276735+Wendigo101@users.noreply.github.com>
2020-11-06 18:14:11 +0100

    Up to mid module 4

--- PredicadoTusMuertos.py
+++ PredicadoTusMuertos.py
@@ -17,6 +17,7 @@ numero=0
 cadena="Hola putos"
 lista=[1,2,3,4]
 lista[3] #Da el tercer elemento de lista
+lista[0] #Da el primer elemento!!!!!!!
 
 tupla=("adios","hola")
 diccionario={"nombre":"puto"}
@@ -81,7 +82,9 @@ False and True # Esto da false
 True and True # Esto da True
 
 
-#####Flujo condicional
+#############################
+##########MODULO 3, FLUJO VARIABLE############
+##########################3##
 
 A=2
 
@@ -125,3 +128,143 @@ while True:
     print("socorro xD")
 print("fin?")
 #Crtx+c to end the loop
+
+####################3
+###################
+
+print(3+4)
+33
+
+print("tus muertos",33)
+print("tus muertos",4,"es mejor que",33) #podrias poner una variable, claro. Para
+#controlar los espacios podrías hacer
+print(f"tus muertos:{4} es mejor que {33}") #no espacio entre : y 4.
+
+
+##########
+
+edad=20
+if edad>=18:
+    print('a beber ijoputa')
+  #print('esto cone spacios mamon') #Esto da error, por tener espacios y no estar a la
+  #par
+    print('esto con espacios hasta alinear')  #esto funciona
+print('end')
+#Ejemplo con funciones
+
+def funcionSuma():
+    a=5
+    b=3
+    print(a+b)
+funcionSuma()
+
+#####################
+##Bloques try y except
+
+try:
+    valor=input("escribe un número: ")
+    valor_numerico=int(valor)
+    resultado=valor_numerico+3
+    print(resultado)
+except Exception as e:
+    print("Hemos sufrido un error debido a que: ",str(e))
+print("fin")
+
+#otra opcion sería en el except redefinir valor_numerico, de modo que si
+#introduces algo malo, uso ese de except. 
+
+#ejemplo discriminando según errores
+
+try:
+    valor=input("escribe un número: ")
+    valor_numerico=int(valor)
+    resultado=3/valor_numerico
+    print(resultado)
+except ArithmeticError:
+    print('Error aritmético (divide by 0)')
+except ValueError: #no has metido numero, sino letras
+    valor_numerico=0
+except Exception as e: #esto es un error mas general
+    print("Hemos sufrido un error debido a que: ",str(e))
+print(valor_numerico)
+print(resultado)
+print("fin")
+
+#Ahora incluimos el bloque finally
+
+try:
+    valor=input("escribe un número: ")
+    valor_numerico=int(valor) #lo convierte de caracteres a numero (un numero)
+    resultado=3/valor_numerico
+    print(resultado)
+except ValueError: #no has metido numero, sino letras
+    valor_numerico=0
+finally:
+    print(valor_numerico)
+print("fin")
+
+#############################
+##########MODULO 4 RESOLVIENDO COSAS############
+##########################3##
+
+#pAR O IMPAR?
+
+num= input("introduce numero, zorra: ") #esto es una cadena
+numInt=int(num) #esto ya sí es un número
+
+if numInt%2==0:
+    print("el número es par")
+else:
+    print("es impar")
+
+#ESCRIBE LOS DIVISBLES ENTRE 5 Y 7 MENORES O IGUALES QUE EL NUMERO(
+#EL NUMERO DEBER SER ENTERO Y POSITIVO)
+
+num= input("introduce numero, zorra: ") #esto es una cadena
+numInt=int(num) #esto ya sí es un número
+#Para hacer un bucle entre el 0 y el numero usaremos range
+list(range(0,numInt)) #no se incluye el número
+for numero in range(0,numInt+1): #el +1 para incluir el numero introducido
+    if numero%5==0 or numero%7==0:
+        print('el número',numero, 'cumple con los requisitos')
+
+#CONTAR LOS ELEMENTOS        
+
+#cpntar elementos de una lsita
+lista=[1,2,3,45,5,6,7,7]
+print(len(lista))
+#Y sin usarla? 
+
+cuenta=0 #inicializacion
+
+for elemento in lista: #Bucle
+    cuenta=cuenta+1
+    print(f'el elemento que estamos añadiendo es el {elemento} y ocupa el orden {cuenta}')
+    
+print('en total hay',cuenta,'elementos') #Fin
+
+#SUMAR VALORES
+lista=[1,2,3,45,5,6,7,7]
+print(sum(lista)) #Esto suma los valores fasi y sensillo
+#Sin usarla, podriamos usar un bucle:
+
+suma=0
+for elemento in lista:
+    suma=suma+elemento
+print('la suma total es',suma)
+
+#CALCULANDO MEDIAS
+ lista=[1,2,3,45,5,6,7,7]
+print(sum(lista)/len(lista)) #Esto da la media de la lista
+
+#Como lo hariamos sin sum ni length?Pues tendríamos que contar los elementos,
+#por otro sumarlos, y listo.
+
+cuenta=0
+suma=0
+
+for elemento in lista:
+    cuenta=cuenta+1 #contar elementos
+    suma=suma+elemento #sumar elementos
+media=suma/cuenta
+print('la media es',media)
